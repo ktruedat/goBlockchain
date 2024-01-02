92c87e345a3968c25fa75d9ac8863232e8bfaf4d
ktruedat <crinnic77@gmail.com>
2024-01-02 15:36:11 +0200

    JSON  marshalling

--- blockchain.go
+++ blockchain.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 )
 
 func main() {
-	blockChain := NewBlockchain()
-	blockChain.Print()
+	//blockChain := NewBlockchain()
+	//blockChain.CreateBlock(5, "hash 1")
+	//blockChain.CreateBlock(3, "hash 2")
+	//blockChain.Print()
+	block := NewBlock(0, "init hash")
+	hash := block.Hash()
+	fmt.Printf("%x", hash)
 
 }
 
@@ -34,6 +42,24 @@ func (b *Block) Print() {
 	fmt.Printf("transactions 		%s\n", b.transactions)
 }
 
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	fmt.Println(string(m))
+	return sha256.Sum256(m)
+}
+
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Timestamp    int64    `json:"timestamp"`
+		Nonce        int      `json:"nonce"`
+		PreviousHash string   `json:"previousHash"`
+		Transactions []string `json:"transactions"`
+	}{Timestamp: b.timpestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions})
+}
+
 type Blockchain struct {
 	transactionPool []string
 	chain           []*Block
@@ -46,10 +72,12 @@ func NewBlockchain() *Blockchain {
 }
 
 func (bc *Blockchain) Print() {
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
 	for i, block := range bc.chain {
-		fmt.Printf("chain %d\n", i)
+		fmt.Printf("%s chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
 		block.Print()
 	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
 }
 
 func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
