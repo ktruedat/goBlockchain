7fa886676075103d12cc22dde929d397b8cc6393
ktruedat <crinnic77@gmail.com>
2024-01-02 13:55:46 +0200

    Block type

--- blockchain.go
+++ blockchain.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("test")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timpestamp   int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	return &Block{
+		nonce:        nonce,
+		previousHash: previousHash,
+		timpestamp:   time.Now().UnixNano(),
+		transactions: nil,
+	}
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp 		%d\n", b.timpestamp)
+	fmt.Printf("previousHash 		%s\n", b.previousHash)
+	fmt.Printf("nonce 			%d\n", b.nonce)
+	fmt.Printf("transactions 		%s\n", b.transactions)
+}
+
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string)
