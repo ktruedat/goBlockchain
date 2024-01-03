d57f960e7abc832067f6b0aa7d82f3562f40811d
ktruedat <crinnic77@gmail.com>
2024-01-03 20:16:57 +0200

    creating blockchain addresses using multiple hashing algorithms

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -6,6 +6,8 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
+	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
 )
 
 type Wallet struct {
@@ -26,12 +28,31 @@ func NewWallet() *Wallet {
 	h2.Write(w.publicKey.Y.Bytes())
 	digest2 := h2.Sum(nil)
 	// 3. Perform RIPEMD-160 hasing on the result of SHA-256 (20 bytes)
+	h3 := ripemd160.New()
+	h3.Write(digest2)
+	digest3 := h3.Sum(nil)
 	// 4. Add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
+	vd4 := make([]byte, 21)
+	vd4[0] = 0x00
+	copy(vd4[1:], digest3[:])
 	// 5. Perform SHA-256 hash on the extended RIPEMD-160 result
+	h5 := sha256.New()
+	h5.Write(vd4)
+	digest5 := h5.Sum(nil)
 	// 6. Perform SHA-256 hash on the result of the previous SHA-256 hash
+	h6 := sha256.New()
+	h6.Write(digest5)
+	digest6 := h5.Sum(nil)
 	// 7. Take the first 4 bytes of the second SHA-256 hash for checksum
+	chsum := digest6[:4]
+
 	// 8. Add the 4 checksum bytes from 7 at the end of extended RIPEMD-160 hash from 4 (25 bytes)
+	dc8 := make([]byte, 25)
+	copy(dc8[:21], vd4[:])
+	copy(dc8[21:], chsum[:])
 	// 9. Convert the result from a byte string into base58
+	addr := base58.Encode(dc8)
+	w.blockchainAddress = addr
 	return w
 }
 
@@ -49,4 +70,8 @@ func (w *Wallet) PublicKey() *ecdsa.PublicKey {
 
 func (w *Wallet) PublicKeyStr() string {
 	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
+}
+
+func (w *Wallet) BlockchainAddress() string {
+	return w.blockchainAddress
 }
