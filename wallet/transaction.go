24b0c4878b61b51f0e86786d4936458cda3b1ab9
ktruedat <crinnic77@gmail.com>
2024-01-04 23:08:04 +0200

    Signature generation using ecdsa algorithm

--- wallet/transaction.go
+++ wallet/transaction.go
@@ -2,7 +2,10 @@ package wallet
 
 import (
 	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
 	"math/big"
 )
 
@@ -20,6 +23,12 @@ func NewTransaction(senderPrivateKey *ecdsa.PrivateKey, senderPublicKey *ecdsa.P
 
 func (t *Transaction) GenerateSignature() *Signature {
 	m, _ := json.Marshal(t)
+	h := sha256.Sum256(m[:])
+	r, s, _ := ecdsa.Sign(rand.Reader, t.senderPrivateKey, h[:])
+	return &Signature{
+		R: r,
+		S: s,
+	}
 
 }
 
@@ -36,4 +45,8 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 type Signature struct {
 	R *big.Int
 	S *big.Int
+}
+
+func (s *Signature) String() string {
+	return fmt.Sprintf("%x%x", s.R, s.S)
 }
