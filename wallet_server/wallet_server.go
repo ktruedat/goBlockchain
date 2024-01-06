5817aee7ae15f003d38cd6fa3883b5b712387c65
ktruedat <crinnic77@gmail.com>
2024-01-06 21:27:42 +0200

    get request api

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type WalletServer struct {
+	port    uint16
+	gateway string
+}
+
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port: port, gateway: gateway}
+}
+
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+func (ws *WalletServer) Gateway() string {
+	return ws.gateway
+}
+
+func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		t, _ := template.ParseFiles("wallet_server/templates/index.html")
+		t.Execute(w, "")
+	default:
+		log.Println("Invalid HTTP method")
+	}
+}
