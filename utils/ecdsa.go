b5ef0888ecf8a45bb759c8960a8a3b967ea784c4
ktruedat <crinnic77@gmail.com>
2024-01-08 21:43:21 +0200

    documenting StringToBigIntTuple

--- utils/ecdsa.go
+++ utils/ecdsa.go
@@ -15,6 +15,8 @@ func (s *Signature) String() string {
 	return fmt.Sprintf("%x%x", s.R, s.S)
 }
 
+// StringToBigIntTuple will take the hexadecimal string representing the X and Y of a
+// public key and return them in a big Int form separately
 func StringToBigIntTuple(s string) (big.Int, big.Int) {
 	bx, _ := hex.DecodeString(s[:64])
 	by, _ := hex.DecodeString(s[64:])
